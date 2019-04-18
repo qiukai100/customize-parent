4ac28486672fc8e08695e0355ac654f262f747d2
qiukai <1035427340@qq.com>
2019-04-18 17:53:32 +0800

    1、增加了hbase客户端

--- teaching-hbase-client/src/main/java/com/customize/hbase/HBaseApplication.java
+++ teaching-hbase-client/src/main/java/com/customize/hbase/HBaseApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.customize.hbase"
+})
 public class HBaseApplication {
 
     public static void main(String[] args) {
--- teaching-hbase-client/src/main/java/com/customize/hbase/controller/PictureController.java
+++ teaching-hbase-client/src/main/java/com/customize/hbase/controller/PictureController.java
@@ -1,5 +1,23 @@
 package com.customize.hbase.controller;
 
+import com.customize.hbase.service.HBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("picture")
 public class PictureController {
 
+    private final HBaseService hBaseService;
+
+    @Autowired
+    public PictureController(HBaseService hBaseService) {
+        this.hBaseService = hBaseService;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public void test() {
+        hBaseService.deleteTable("test_base");
+    }
 }
