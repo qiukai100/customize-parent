85dcd166bc1ea3f13dd01c58cf1367a13f448cc8
qiukai <1035427340@qq.com>
2019-04-19 17:07:18 +0800

    1、增加了feign多文件上传的解决方案

---
+++ teaching-component/src/main/java/com/customize/component/config/FeignMultipartConfig.java
@@ -0,0 +1,22 @@
+package com.customize.component.config;
+
+import com.customize.component.modules.FeignSpringFormEncoder;
+import feign.codec.Encoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class FeignMultipartConfig {
+
+    @Bean
+    @Primary
+    @Scope("prototype")
+    public Encoder multipartFormEncoder(ObjectFactory<HttpMessageConverters> messageConverters) {
+        return new FeignSpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+}
