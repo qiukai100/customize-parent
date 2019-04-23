fe09daec64455de5149faa88eac96fdf41323ee8
qiukai <1035427340@qq.com>
2019-04-23 21:12:35 +0800

    1、优化了图片上传功能

--- teaching-hbase-client/src/main/java/com/customize/hbase/utils/PictureUtil.java
+++ teaching-hbase-client/src/main/java/com/customize/hbase/utils/PictureUtil.java
@@ -1,10 +1,6 @@
 package com.customize.hbase.utils;
 
 import com.customize.hbase.constants.ColumnFamilyType;
-import org.apache.commons.io.IOUtils;
-import sun.misc.BASE64Encoder;
-
-import java.io.FileInputStream;
 
 public class PictureUtil {
 
@@ -22,30 +18,5 @@ public class PictureUtil {
      */
     public static String pathToUrl(String tableName, String rowKey, String columnName) {
         return tableName.concat(ID_BLOCK).concat(rowKey).concat(ID_BLOCK).concat(ColumnFamilyType.IMAGE.name()).concat(ID_BLOCK).concat(columnName);
-    }
-
-
-    /**
-     * 文件流转byte
-     * @param fileInputStream 文件流
-     * @param length 长度
-     * @return
-     */
-    public static String getFileByte(FileInputStream fileInputStream, int length) {
-        String fileStr = "";
-        try {
-            byte[] bytes = new byte[length];
-            int len = 0;
-            while (len != -1) {
-                len = fileInputStream.read(bytes);
-            }
-            BASE64Encoder be = new BASE64Encoder();
-            fileStr = be.encode(bytes);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            IOUtils.closeQuietly(fileInputStream);
-        }
-        return fileStr;
     }
 }
