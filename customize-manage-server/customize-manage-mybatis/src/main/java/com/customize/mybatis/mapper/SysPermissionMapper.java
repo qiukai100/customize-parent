6359fb28b327a9399605240b257f3be16b3138ab
qiukai <qiukaijobemail@163.com>
2020-04-25 15:13:28 +0800

    1、修改activiti 2、修改自动生成代码

--- customize-manage-server/customize-manage-mybatis/src/main/java/com/customize/mybatis/mapper/SysPermissionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.customize.mybatis.mapper;
-
-import com.customize.domain.entity.sys.Permission;
-import com.customize.mybatis.core.BaseMapper;
-
-public interface SysPermissionMapper extends BaseMapper<Permission> {
-
-}
